65a46c5bf896aa9c19b9dbe5d1b943318c8d6639
unknown <kartiks04800@gmail.com>
2025-07-29 20:33:12 +0530

    setup_vercel_backend

---
+++ backend/api/routes/route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  return res.status(200).json({ message: "BFHL API is working." });
+});
+
+
+
+module.exports = router;
