e284bc4c32062644b5487c391ab362c8c1f98952
unknown <kartiks04800@gmail.com>
2025-07-29 21:28:38 +0530

    commented_for_local_host

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use('/api', router); 
 
+// app.listen(3000,()=>{
+//     console.log('Server is running on port 3000');
+// })
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
